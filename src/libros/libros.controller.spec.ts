07287afc357d7062dc963531cd7f6744fdd0361f
JobV <jobvillagran@gmail.com>
2023-08-31 15:14:11 -0600

    mod[swagger]

--- src/libros/libros.controller.spec.ts
+++ src/libros/libros.controller.spec.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Libro } from './libro.entity';
+import { Usuario } from 'src/usuarios/usuario.entity';
 
 @Injectable()
 export class LibrosService {
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as express from 'express';
+import createRedocMiddleware from 'redoc-express'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Configuración de Swagger
-  const options = new DocumentBuilder()
+  const swaggerOptions = new DocumentBuilder()
     .setTitle('NAO API')
     .setDescription('API de ejemplo para NAO')
     .setVersion('1.0')
     .addTag('Libros')
     .addTag('Usuarios')
     .build();
-  const document = SwaggerModule.createDocument(app, options);
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
 
-  SwaggerModule.setup('api-docs', app, document);
+  // Configuración de ReDoc
+  const redocOptions = {
+    title: 'NAO API Documentation',
+    specUrl: '/api-docs/json', // URL de la especificación OpenAPI generada por Swagger
+  };
+  const redocApp = express();
+  const redocMiddleware = createRedocMiddleware(redocOptions); // Usar createRedocMiddleware
+  redocApp.use(redocMiddleware);
+  app.use('/redoc', redocApp);
+
+  // Habilitar CORS para ReDoc
+  app.enableCors();
+
+  // Configurar Swagger
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
 
   await app.listen(3000);
 }
